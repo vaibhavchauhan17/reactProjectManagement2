821eb43ce5905e41134cba1c93600c29608aae4d
Vaibhav Kumar Chauhan <vaibhavchauhan17>
2024-01-05 22:28:56 +0530

    adding functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,54 @@
+import { useState } from "react";
+
 import NewProject from "./components/NewProject.jsx";
 import NoProjectsSelected from "./components/NoProjectsSelected.jsx";
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 
 function App() {
+
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject(){
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null
+      }
+    });
+  }
+
+  function handleAddProject(projectData){
+    setProjectsState(prevState =>{
+
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      };
+
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject]
+      };
+    });
+  }
+
+
+  let content;
+
+  if(projectsState.selectedProjectId === null){
+    content=<NewProject onAdd={handleAddProject}/>
+  }else if(projectsState.selectedProjectId === undefined){
+    content = <NoProjectsSelected onStartAddProject={handleStartAddProject}/>
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NoProjectsSelected />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects}/>
+      {content}
     </main>
   );
 }
